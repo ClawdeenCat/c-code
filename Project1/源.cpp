1c9abce71a9cc61821d22d39c3a060b4a41472fe
ClawdeenCat <63696351+ClawdeenCat@users.noreply.github.com>
2022-09-19 16:11:06 +0800

    Add files via upload

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#pragma warning(disable:4996) //忽略4996警告
 
 int main(void)
 {
-	printf("hello");
-	return 0;
+	int a,b,c;
+	scanf("%d,%d,%d",&a, &b, &c);
+	printf("%d,%d,%d", a, b, c);
+	return 0;//结束进程
 }
+
+
+
