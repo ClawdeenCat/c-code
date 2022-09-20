d5df2f6f669191a2dc4d6c492b2840ea8f848460
ClawdeenCat <63696351+ClawdeenCat@users.noreply.github.com>
2022-09-20 16:47:32 +0800

    Add files via upload

---
+++ Project3/源.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+
+
+int main()
+{
+
+	/********************************
+	float a = 15443545.55f;
+	float b = 24142.1330f;
+		scanf("%f%f", &a, &b);
+	printf("%f\n", a);
+	printf("%f", b);
+
+	********************************/
+
+
+
+
+
+	//printf("%12.5e", 30.253);
+
+
+
+
+
+	/********************************
+	int day,mon,year;
+	printf("Please Enter a Date:");
+	scanf("%d/%d/%d", &day, &mon, &year);
+	printf("Your Date is:%d/%d/%d",day,mon,year);
+	********************************/
+
+
+
+
+
+
+
+
+
+
+	/********************************
+	int b = 3;
+	float a= 3.0f;
+	a = a++*4;
+	printf("%f\n",a);
+	printf("%9.2d", b);
+	********************************/
+
+	 
+
+
+	if (1 + 1 != 3 && 1+1==5)
+		printf("yes");
+
+	else
+		printf("no");
+
+	
+
+
+
+
+
+
+	return 0;
+}
